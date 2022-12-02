9508775bb32025e8688adbc18fff7494dc58474e
VFM | SB <vfm_sb@proton.me>
2022-12-02 22:57:35 +0200

    Added Metadata to hangman.py

---
+++ hangman.py
@@ -0,0 +1,10 @@
+"""Hangman CLI"""
+
+__author__ = "VFM | SB"
+__email__ = "vfm_sb@proton.me"
+__copyright__ = "Copyleft 2022"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "VFM | SB"
+__status__ = "Development"
+
