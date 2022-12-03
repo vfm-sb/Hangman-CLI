566faef17bc2b8a1ba3396d2388961f45662608b
VFM | SB <vfm_sb@proton.me>
2022-12-03 23:10:30 +0200

    Some UI Optimisations Before First Push

--- hangman.py
+++ hangman.py
@@ -4,10 +4,11 @@ __author__ = "VFM | SB"
 __email__ = "vfm_sb@proton.me"
 __copyright__ = "Copyleft 2022"
 __license__ = "MIT"
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __maintainer__ = "VFM | SB"
 __status__ = "Development"
 
+import os # Built-in Modules
 
 from helpers import read_file # Local Modules
 from helpers import random_word
@@ -15,7 +16,7 @@ from art import LOGO, STAGES
 
 
 # Functions
-def repr_secret_word(secret_word: list) -> None:
+def repr_word(secret_word: list) -> None:
     word = [char for char in secret_word]
     word_length = len(word)
     edge = f'+{"-" * (word_length * 2 + 1)}+'
@@ -30,19 +31,23 @@ def main():
     word_length = len(chosen_word)
     secret_word = ["_" for _ in range(word_length)]
     # test
-    print(f"Solution is {chosen_word}")
-    repr_secret_word(secret_word)
+    # print(f"Solution is {chosen_word}")
+    repr_word(secret_word)
+    print()
     end_of_game = False
     lives = 7
     chosen_chars = []
     while not end_of_game:
         chosen_char = input("Guess a Letter: ").lower()
+        os.system("clear")
         # check if character already guessed before:
         if (
             chosen_char in secret_word
             or chosen_char in chosen_chars
         ):
-            print(f'You\'ve already guessed letter {chosen_char}\n')
+            print(f'You\'ve Already Guessed Letter "{chosen_char}" Before!\n')
+            repr_word(secret_word)
+            print(STAGES[lives])
             continue
         chosen_chars.append(chosen_char)
         occurances = 0
@@ -54,29 +59,31 @@ def main():
         # reduces lives by 1
         if chosen_char not in chosen_word:
             print(
-                f'The letter "{chosen_char}" isn\'t found! '
+                f'The Letter "{chosen_char}" was not Found! '
                 "You've Lost a Life!"
             )
             lives -= 1
         else:
             print(
-                f'The Letter "{chosen_char}" is Found in {occurances} '
+                f'The Letter "{chosen_char}" was Found in {occurances} '
                 f'{"Place" if occurances < 2 else "Different Places"}.'
             )
         # if no more lives left, end_of_game = True,
         # game is lost
         if lives == 0:
             end_of_game = True
-            print("You Lost!")
+            print("You've Lost!")
+            print()
             print("The Word was:")
-            repr_secret_word(list(chosen_word))
+            repr_word(list(chosen_word))
         else:
-            repr_secret_word(secret_word)
+            print()
+            repr_word(secret_word)
         # if all characters guessed correctly, end_of_game = True,
         # game is won
         if "_" not in secret_word:
             end_of_game = True
-            print("You've won the game!")
+            print("You've Won!")
         # display respective hangman art
         print(STAGES[lives])
 
